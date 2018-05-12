57b2c1f8682cb736ef0b6109bd1dec208dac5fe1
Mumtahin Habib <38589844+mhrafi66@users.noreply.github.com>
2018-05-12 20:59:28 +0600

    Update SIS.cpp
    main function and input is complete

--- SIS.cpp
+++ SIS.cpp
@@ -6,10 +6,11 @@
 #include<conio.h>
 #include<time.h>
 #include<windows.h>
+#include<string>
 #define FILE_NOT_FOUND -1;
 #define USER_NOT_FOUND -2;
-//#define SUCCESS 2;
-#difine ADM_PASS "IUT"
+#define SUCCESS 2;
+//#difine ADM_PASS "DUNNO"
 using namespace std;
 
 inline void clrscr(){
@@ -29,8 +30,160 @@ void getdate(char *tday){
 	struct tm *d=localtime(&tim);
 	sprintf(tday,"%02d/%02d/%d",d->tm_mday,d->tm_mon+1,d->tm_year+1900);
 }
+string department[8];
+void setdepartment(){
+   department[1]="Mechanical and Chemical Engineering (MCE)";
+    department[2]="Electrical and Electronic Engineering(EEE)";
+    department[3]="Technical and Vocational Education(TVE)";
+    department[4]="Computer Science and Engineering (CSE)";
+    department[5]="Civil and Environmental Engineering(EEE)";
+    department[6]="Software Engineering (SWE)";
+    department[7]="Business and Technology Management(BTM)";
+
+}
+class student;
+class staff{
+    protected:
+        char staff_id[10];
+        char staff_name[50];
+        char email[30];
+    public:
+        staff(){
+            staff_id[0]=staff_name[0]=email[0]='\0';
+        }
+        staff(char *id,char *name,char *mail){
+            strcpy(staff_id,id);
+            strcpy(staff_name,name);
+            strcpy(email,mail);
+        }
+        char* getid(){
+            return staff_id;
+        }
+        int init(char*);
+        virtual void showProfile(){
+        clrscr();
+        title();
+        cout<<"Profile...."<<endl;
+        cout<<"_______________________________________________________________________________"<<endl;
+        cout<<" Staff Name      \t :"<<staff_name<<endl;
+        cout<<" Staff Id        \t :"<<staff_id<<endl;
+        cout<<" Email id        \t :"<<email<<endl;
+        cout<<"_______________________________________________________________________________"<<endl;
+        fflush(stdin);
+        cin.get();
+        }
+};
+class admin;
+class teachar:public staff{
+        char password[10];
+public:
+        teacher(){
+        
+        }
+        char* getid(){
+				return staff_id;
+			}
+		 int init(char*);
+         int manageAttendence();
+         int manageMarks();
+
+};
+class admin:public staff{
+        char password[10];
 
 
+};
+
+int setup(){
+	title();
+	ifstream fp("student.bin",ios::binary|ios::in);
+	if(fp.is_open())
+		return 0;
+	cout<<endl<<"Initiating required files for first time usage... ";
+    fp.close();
+	ofstream ofp("student.bin",ios::binary|ios::out);
+// Store the initial data to the file.
+	if(!ofp.is_open()) return -1;
+	ofp.close();
+	ofstream _ofp("staff.bin",ios::binary|ios::out);
+	if(!_ofp.is_open()) return -1;
+	_ofp.close();
+	ofstream _o("work_date.bin",ios::out);
+	_o.close();
+	ofstream _f("notification.bin",ios::binary|ios::out);
+	if(!_f.is_open()) return -1;
+	_f.close();
+	ofstream _fi("complaint.bin",ios::binary|ios::out);
+	if(!_fi.is_open()) return -1;
+	_fi.close();
+	ofstream _fii("topic.bin",ios::binary|ios::out);
+	if(!_fii.is_open()) return -1;
+	_fii.close();
+	ofstream _fiii("subject_code.bin",ios::binary|ios::out);
+	if(!_fiii.is_open()) return -1;
+	_fiii.close();
+	Sleep(2000);
+	cout<<" : DONE";
+	cin.get();
+	return 0;
+}
+int student(){
+    return 0;
+};
+int teacher(){
+    return 0;
+};
+int admin(){
+    return 0;
+};
 int main(){
+    setdepartment();
+    if(setup()) {
+		cout<<"Files are missing and unable to create new files...\n Please try again..."; Sleep(2000);
+		exit(1);
+	}
+    while(1){
+        clrscr();
+        title();
+        cout<<"\n\t\tLogin Option for you"<<endl;
+        cout<<"\t\t 1 : Student "<<endl;
+        cout<<"\t\t 2 : Teacher   "<<endl;
+        cout<<"\t\t 3 : Admin "<<endl;
+        cout<<"\t\t 4 : Exit"<<endl;
+        cout<<endl<<"\t\tEnter your choice : ";
+        static int error;
+        int your_choice;
+        cin >> your_choice;
+        switch(your_choice){
+            case 1 : error=0;
+					while(error<5) {  //Students are allowed to attemp for maximum 5 times
+					        if(student())
+							error++;
+                  			else break;
+								   }
+					if(error>4)
+							cout<<" Authentication failed...";
+				 			cin.get();
+					break;
+            case 2 :error=0;
+					while(error<5) {
+                            if(teacher())
+							error++;
+                  			else break;
+								   }
+                    if(error>4)
+                        cout<<" Authentication failed...";
+                    cin.get();
+					break;
+            case 3 :admin();
+                	break;
+            case 4 :cout<<"Terminating..."; Sleep(1000);
+                    exit(0);
+            default:cout <<"Invalid choice : ";
+                    Sleep(1000);
+                    cin.get();
+
+        }
+    }
 
 }
