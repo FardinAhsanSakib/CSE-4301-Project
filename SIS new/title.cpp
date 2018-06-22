5a5f15e1515ca613129156b219a71143f801979c
Mumtahin Habib <38589844+mhrafi66@users.noreply.github.com>
2018-06-22 17:32:53 +0600

    title and another function to get date from os

---
+++ SIS new/title.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<cstdio>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+
+void title(){
+    clrscr();
+	cout<<"==============================================================================="<<endl;
+	cout<<"**************           Student Information System           *****************"<<endl;
+	cout<<"**************      Islamic University of Technology (IUT)    *****************"<<endl;
+	cout<<"==============================================================================="<<endl;
+}
