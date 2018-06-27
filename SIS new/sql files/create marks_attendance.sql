b7f311f74d1335183ba680704853b5cd44276e49
Mumtahin Habib <38589844+mhrafi66@users.noreply.github.com>
2018-06-27 06:43:51 +0600

    adding a new column to store academic year

--- SIS new/sql files/create marks_attendance.sql
+++ SIS new/sql files/create marks_attendance.sql
@@ -6,6 +6,7 @@ create table marks_attendance(
 	marks number not null,
 	tot_cls number not null,
 	attend number not null,
+	aca_year varchar2(50) not null,
 	constraints fk_marks_stu foreign key(sid) references student(id),
 	constraints fk_marks_sub foreign key(scode) references subject(scode),
 	constraints fk_marks_dept foreign key(dept) references department(id)
