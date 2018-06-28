b56a6f0cc1ad1a233d17b96014394631d9095cad
Mumtahin Habib <38589844+mhrafi66@users.noreply.github.com>
2018-06-28 21:46:09 +0600

    declaring all the functions which would be appear to user

---
+++ SIS new/user_func.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<cstdio>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+int user_student();
+int user_teacher();
+int user_admin();
+int setup();
